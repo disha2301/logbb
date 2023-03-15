8b2187015b282e67aede69e9a7848b250c792708
Disha <dishay166@gmail.com>
2023-03-15 23:56:02 +0530

    :tada: just begun

---
+++ components/footer.js
@@ -0,0 +1,8 @@
+
+export default function footer() {
+  return (
+    <div className="text-3xl">
+      footer
+    </div>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import Format from '@/layout/format'
+
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <Format>
+     <h1>heyyyyy</h1>
+    </Format>
+  )
+}

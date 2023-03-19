df0e316f6471eb0bda45ce93b4b4436e82823343
Disha <dishay166@gmail.com>
2023-03-20 00:29:29 +0530

    :art: working

--- components/header.js
+++ components/header.js
@@ -7,7 +7,7 @@ export default function header() {
   return (
     <header className="bg-gray-50">
         <div className="xl:container xl:mx-auto flex flex-col items-center sm:flex-row sm:justify-between text-center py-3">
-            <div className="md:flex-none w-96 order-2 sm:order-1 py-4 justify-center sm:py-0">
+            <div className="md:flex-none w-96 order-2 sm:order-1 py-4 justify-center sm:py-0 px-6">
                 <input type="text" className="input-text" placeholder="Search..."/>
             </div>
             <div className="shrink w-80 sm:order-2">
--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,16 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Format from '@/layout/format'
 
+//components
+import Section1 from '../components/section1'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <Format>
-     <h1>heyyyyy</h1>
+     <Section1></Section1>
     </Format>
   )
 }

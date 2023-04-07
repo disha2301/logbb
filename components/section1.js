77b06a8a71010ac17fe9a58c836250ba6abb0248
Disha <dishay166@gmail.com>
2023-04-07 11:52:19 +0530

    :bug: fixed

--- components/section1.js
+++ components/section1.js
@@ -15,14 +15,21 @@ function Slide(){
     return(
         <div className="grid md:grid-cols-2">
             <div className="image">
-                <Link legacyBehavior href={"/"}><a><Image src={"/images/image1.jpeg"} width={600} height={600}/></a></Link>
+                <Link href={"/"}><Image src={"/images/image1.jpeg"} width={600} height={600}/></Link>
                 
 
             </div>
-            <div className="info">
+            <div className="info flex justify-center flex-col">
                 <div className="category">
-                    <Link href={"/"}><a className="text-orange-600 hover:text-orange-800">Business,Travel</a></Link>
+                    <Link href={"/"} className="text-orange-600 hover:text-orange-800">Business,Travel</Link>
+                    <Link href={"/"} className="text-gray-600 hover:text-gray-800">-July 3, 2023</Link>
                 </div>
+                <div className="title">
+                    <Link href="/" className="text-3xl md:text-6xl font-bold text-gray-800 hover:text-gray-600">Blockchain technology is an advanced database mechanism</Link>
+                    <p className="text-gray-500 py-3">There are four main types of blockchain networks: public blockchains, private blockchains, consortium blockchains and hybrid blockchains.</p>
+                    <h1>author</h1>
+                </div>.
+
 
             </div>
 

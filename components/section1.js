e79b0194a730821ea3b46a7f68480d0171ad41ee
Disha <dishay166@gmail.com>
2023-04-07 13:34:21 +0530

    :art: swipper added

--- components/section1.js
+++ components/section1.js
@@ -1,8 +1,15 @@
 import Image from "next/image"
 import Link from "next/link"
 import Author from "./_child/author"
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import SwiperCore,{Autoplay} from "swiper";
 
 export default function section1() {
+
+    SwiperCore.use([Autoplay])
     const bg = {
         background: "url('/images/bg.jpg') no-repeat",
         backgroundPosition: "right",
@@ -12,7 +19,22 @@ export default function section1() {
    <section className="py-16" style={bg}>
     <div className="container mx-auto md:px-20">
          <h1 className="font-bold text-4xl pb-12 text-center">Trending</h1>
-         {Slide()}
+         <Swiper
+            slidesPerView={1}
+            loop={true}
+            autoplay={{
+                delay:4000
+            }}
+            >
+
+            <SwiperSlide>{Slide()}</SwiperSlide>
+            <SwiperSlide>{Slide()}</SwiperSlide>          
+            <SwiperSlide>{Slide()}</SwiperSlide>
+            <SwiperSlide>{Slide()}</SwiperSlide>          
+            <SwiperSlide>{Slide()}</SwiperSlide>
+            ...
+    </Swiper>
+         
     </div>
    </section>
   )
